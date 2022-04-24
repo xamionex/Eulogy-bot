e715d7bb2f52cf9f9d797744ff56f48233ec94c7
pyDebugger <57315331+CrowDev0@users.noreply.github.com>
2022-04-24 13:49:44 +0200

    Code Rewriting and Eulogy Commands
    Removed wikisearch, membercount, and servercount, replaced jokes list, made code more readable, added comments, added eulogy commands, fixed one or two typos, removed a bunch of unused imports, fixed commands not working, and added automatic responses

--- main.py
+++ main.py
@@ -1,186 +1,191 @@
-from ast import Delete
-from email import message
-from logging import warning
-from tkinter import N
-from turtle import color
-from unicodedata import name
 import discord
 from discord.ext import commands
 import datetime
-from urllib import parse, request
-import re
 import random
-import time
-from wikipedia import summary
-import os
+import json
 
-startinputstat = input("Status: ")
-prefixvarinput = input("Choose a prefix: 1 = amogus, 2 = $, 3 = eulogy ")
-if prefixvarinput == "1":
-    prefixvar = "amogus"
-elif prefixvarinput == "2":
-    prefixvar = "$"
-elif prefixvarinput == "3":
-    prefixvar = "!"
-else:
-    print("You must choose between numbers: 1, 2, and 3. (This window will close in 5 seconds.)")
-    time.sleep(5)
-    quit()
-
-print("One second...")
-time.sleep(3)
-print("Ready to deploy!")
-time.sleep(.5)
-print("Deploying...")
-time.sleep(2)
-print("Deployed!")
-print("Notice! Closing this window will turn off the bot.")
-activity = discord.Game(name=startinputstat)
-jokelist = ["you <:HDtroll:943715101708529767>", "Earlier today, I installed Astroneer on my AWS server to simulate blowing up 30,000 dynamite at once.  Now the world is down 1 server provider.", " If a picture is worth a thousand words, does that mean I can hire a photographer by speaking to them a bunch?", "what sucks more than my life? the desk plant", "what sucks more than my life? heretic with several stacks of bungus"]
-coins = ["It landed on heads!", "It landed on tails!"]
+# set up discord related stuff
+prefixvar = "$"
+activity = discord.Game(name="with your mother | $help")
 client = discord.Client()
-bot = commands.Bot(command_prefix=prefixvar,
-                   description="This is a Bot made by wiki.",
-                   help_command=None, activity=activity, status=discord.Status.online)
+bot = commands.Bot(
+    command_prefix=prefixvar,
+    description="This is a Bot made by Wiki and Crow.",
+    help_command=None,
+    activity=activity,
+    status=discord.Status.online
+)
 
-@bot.event
-async def on_ready():
-    print("Online.")
-    print("Awaiting commands.")
+# set up non-discord related stuff
+eulogy_emoji = "<:eulogy_zero:967096744800296970>" # 967096744800296970 when running, 967740312543965224 when testing
 
-@bot.command()
-async def ping(ctx):
-    await ctx.message.channel.send('pong')
-    print("Ping command used in: " + ctx.guild.name)
+coins = ["It landed on heads!", "It landed on tails!"]
+jokes = [    
+    "Alien Head."
+]
 
-@bot.command()
-async def eulogy(ctx):
-    await ctx.message.channel.send("<:eulogy_zero:967096744800296970>")
-
-@bot.command()
-async def info(ctx):
-    embed = discord.Embed(title=f"{ctx.guild.name}", description="Server information",
-                          timestamp=datetime.datetime.utcnow(), color=0xf42069)
-    embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
-    embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
-    embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
-    embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
-    embed.set_thumbnail(url="https://img.birb.cc/YCYqJsSu.png")
-
-    await ctx.message.channel.send(embed=embed)
-    print("Info was sent in: " + ctx.guild.name)
+# commands
 
 @bot.command()
-async def kick(ctx, member: discord.Member, *, reason=None):
+async def eulogy(ctx): # send eulogy
+    await ctx.message.reply(eulogy_emoji)
+
+@bot.command()
+async def eulogycount(ctx): # count the total amount of times eulogy has been said
+    # count
+    counter = 0
+
+    for channel in ctx.guild.text_channels:
+        async for message in channel.history(limit=None):
+            if "eulogy" in message.content.lower():
+                counter += 1
+
+    await ctx.message.reply(str(counter) + " times.")
+
+@bot.command()
+async def kick(ctx, member: discord.Member, *, reason=None): # kick member
     if ctx.message.author.guild_permissions.kick_members:
         await member.kick(reason=reason)
-        embed = discord.Embed(title="user kicked", description="kicked user " + str(member),
-            timestamp=datetime.datetime.utcnow(), color=0xf42069)
+
+        embed = discord.Embed(
+            title="User Kicked",
+            description="Kicked user " + str(member),
+            timestamp=datetime.datetime.utcnow(),
+            color=discord.Color.red()
+        )
+
         embed.add_field(name="Reason", value=str(reason))
+
         await ctx.message.channel.send(embed=embed)
         await ctx.message.delete()
-        print("Kicked member: " + str(member) + ". Report from: " + ctx.guild.name)
     else:
         await ctx.message.channel.send("You do not have permission to kick.")
-        print(
-            "Member attempted to kick. Did not have admin, or bot's role was obstructed. Report from: " + ctx.guild.name)
 
 @bot.command()
-async def ban(ctx, member: discord.Member, *, reason=None):
+async def ban(ctx, member: discord.Member, *, reason=None): # ban member
     if ctx.message.author.guild_permissions.ban_members:
         await member.ban(reason=reason)
-        embed = discord.Embed(title="user banned", description="banned user " + str(member),
-            timestamp=datetime.datetime.utcnow(), color=discord.Color.red())
+
+        embed = discord.Embed(
+            title="User Banned",
+            description="Banned user " + str(member),
+            timestamp=datetime.datetime.utcnow(),
+            color=discord.Color.red()
+        )
+
         embed.add_field(name="Reason", value=str(reason))
+
         await ctx.message.channel.send(embed=embed)
         await ctx.message.delete()
-        print("Banned member: " + str(member) + ". Report from: " + ctx.guild.name)
     else:
         await ctx.message.channel.send("You do not have permissions to ban.")
-        print(
-            "Member attempted to ban. Did not have admin, or bot's role was obstructed. Report from: " + ctx.guild.name)
+
+@bot.command()
+async def coinflip(ctx): # flip a coin
+    await ctx.message.reply(random.choice(coins))
+
+@bot.command()
+async def joke(ctx): # send a random joke out of the list
+    await ctx.message.reply(random.choice(jokes))
+
+@bot.command()
+async def jokerep(ctx, *, jrep): # suggest a joke to add to the bot
+    await ctx.message.channel.send(
+        "Your joke was recorded. It will be added if Wiki approves it.")
+    jokefile = open("jokes.txt", "a")
+    jokefile.write("\n" + jrep + "\n")
+    jokefile.close
+    del jokefile    
 
 @bot.command()
 async def fban(ctx, member: discord.Member, *, reason=None):
     if ctx.message.author.guild_permissions.ban_members:
         embed = discord.Embed(title="user banned", description="banned user " + str(member),
             timestamp=datetime.datetime.utcnow(), color=0xf42069)
+        embed = discord.Embed(
+            title="User Banned",
+            description="Banned user " + str(member),
+            timestamp=datetime.datetime.utcnow(),
+            color=discord.Color.red()
+        )
+
         embed.add_field(name="Reason", value=str(reason))
+
         await ctx.message.channel.send(embed=embed)
         await ctx.message.delete()
-        print("FakeBanned member: " + str(member) + ". Report from: " + ctx.guild.name)
     else:
         await ctx.message.channel.send("You do not have permissions to ban.")
-        print(
-            "Member attempted to fakeban. Did not have ban perms. Report from: " + ctx.guild.name)
 
 @bot.command()
-async def coinflip(ctx):
-    await ctx.message.channel.send(random.choice(coins))
-    print("Coin flip used in: " + ctx.guild.name)
+async def help(ctx): # creates an embed with help for each command
+    embed = discord.Embed(
+        title="Command Help",
+        description="Help for all current commands this discord bot supports.",
+        color=0xf42069
+    )
 
-@bot.command()
-async def joke(ctx):
-    await ctx.message.channel.send(random.choice(jokelist))
-    print("Joke sent in: " + ctx.guild.name)
+    embed.add_field(
+        name=f"{prefixvar}ban [member]",
+        value="Bans a user.", 
+        inline=False
+    )
 
-@bot.command()
-async def jokerep(ctx, *, jrep):
-    await ctx.message.channel.send(
-        "Your joke was recorded. It will be added if Wiki aporoves it.")
-    jokefile = open("jokes.txt", "a")
-    jokefile.write("\n" + jrep + "\n")
-    jokefile.close
-    print("Incoming joke from: " + ctx.guild.name + " Joke sent: " + jrep)
+    embed.add_field(
+        name=f"{prefixvar}kick [member]",
+        value="Kicks a user.", 
+        inline=False
+    )
+
+    embed.add_field(
+        name=f"{prefixvar}coinflip",
+        value="Randomly picks between heads, and tails.", 
+        inline=False
+    )
+
+    embed.add_field(
+        name=f"{prefixvar}joke",
+        value="Randomly picks a joke from my joke database. You can recommend a joke using the jokerep command.",
+        inline=False)
+
+    embed.add_field(
+        name=f"{prefixvar}jokerep [joke]",
+        value="Adds a joke to the bot's database, to be used for the joke command.",
+        inline=False
+    )
+
+    embed.add_field(
+        name=f"{prefixvar}eulogy",
+        value=eulogy_emoji,
+        inline=False
+    )
+
+    embed.add_field(
+        name=f"{prefixvar}eulogycount",
+        value="Count how many times eulogy has been said in any way.",
+        inline=False
+    )
+
+    embed.set_footer(
+        text=f"Command missing? message NotAPro#9901 for help or go to bot help. Made by Wiki and Crow. (Command prefix: {prefixvar})"
+    )
+
+    await ctx.send(embed=embed)
+
+# events
+@bot.event
+async def on_ready():
+    print("Connected.")
+    print("Awaiting commands.")
 
 @bot.event
 async def on_message(message):
+    # automatic responses
     if "eulogy" in message.content.lower():
-            emoji = "<:eulogy_zero:967096744800296970>"
-            await message.add_reaction(emoji)
+        await message.add_reaction(eulogy_emoji)
+    if "cleansing pool" in message.content.lower():
+        await message.reply("Watch your language.")
 
-@bot.command()
-async def membercount(ctx):
-    await ctx.message.channel.send("There are " + str(ctx.guild.member_count) + " members in this server.")
-    print("Member count command used in server: " + ctx.guild.name)
+    await bot.process_commands(message) # fix commands not working
 
-@bot.command()
-async def wikisearch(ctx, *, search):
-    try:
-        searchresults = summary(str(search), sentences=15)
-        await ctx.channel.send(searchresults)
-        print("Wikipedia command used. Search term used: " + str(search) + ". Report from: " + ctx.guild.name)
-    except:
-        print("Wikisearch error from: " + ctx.guild.name)
-        await ctx.channel.send("Uh-oh, something went wrong! An automatic report was sent to Wiki.")
-
-@bot.command()
-async def help(ctx):
-    embed = discord.Embed(title="Command Help", description="Help for all current commands this discord bot supports.",
-                          color=0xf42069)
-    embed.add_field(name=f"{prefixvar}ping", value="Replies with pong.", inline=False)
-    embed.add_field(name=f"{prefixvar}info", value="Gives info about the server.", inline=False)
-    embed.add_field(name=f"{prefixvar}ban [member]", value="Bans a user.", inline=False)
-    embed.add_field(name=f"{prefixvar}kick [member]", value="Kicks a user.", inline=False)
-    embed.add_field(name=f"{prefixvar}coinflip", value="Randomly picks between heads, and tails.", inline=False)
-    embed.add_field(name=f"{prefixvar}joke",
-                    value="Randomly picks a joke from my joke database. You can reccomend a joke using the jokerep command.",
-                    inline=False)
-    embed.add_field(name=f"{prefixvar}jokerep [joke]",
-                    value="Adds a joke to the bot's database, to be used for the joke command.", inline=False)
-    embed.add_field(name=f"{prefixvar}membercount", value="Tells you the amount of members in a server.", inline=False)
-    embed.add_field(name=f"{prefixvar}wikisearch [Search term]",
-                    value="Searchs Wikipedia for information, and displays it.", inline=False)
-    embed.set_footer(
-        text=f"Command missing? message NotAPro#9901 for help or go to bot help. Made by Wiki. (Command prefix: {prefixvar})")
-    await ctx.send(embed=embed)
-    print("Help command used in: " + ctx.guild.name)
-
-# Events and stuff
-
-@bot.event
-async def on_guild_join(guild):
-    servers = list(bot.guilds)
-    print("We are in a new server! I am in a total of: " + str(len(servers)) + " servers!")
-
+print("Notice! Closing this window will turn off the bot.")
 bot.run('token goes here')
