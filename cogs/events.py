b0a47dc7d39aca1e48aebe19fa2416d6a1d06c25
amione <percesvrzic@gmail.com>
2022-05-29 00:02:52 +0200

    rewrite for configs and individual triggers

--- cogs/events.py
+++ cogs/events.py
@@ -1,7 +1,7 @@
 import main
 import discord
 from discord.ext import commands, tasks
-from cogs import utils as utilz, other
+from cogs import utils, users, configs
 import json
 import random
 import time
@@ -20,12 +20,6 @@ class Events(commands.Cog, name="Events"):
 
     def __init__(self, bot):
         self.bot = bot
-        self.save = bot.save
-        self.hugs = bot.hugs
-        self.convo_replies = bot.convo_replies
-        self.eulogy_emoji = bot.eulogy_emoji
-        self.lunar_coin_emoji = bot.lunar_coin_emoji
-        self.lunar_symbol = bot.lunar_symbol
         self.autosave.start()
 
     def cog_unload(self):
@@ -41,11 +35,11 @@ class Events(commands.Cog, name="Events"):
         if isinstance(error, commands.BotMissingPermissions):
             raise error
         elif isinstance(error, commands.CommandOnCooldown):
-            e = discord.Embed(description=error, color=0xFF6969)
-            await utilz.sendembed(bot, e, show_all=False)
+            e = discord.Embed(description=f"`❌` {error}", color=0xFF6969)
+            await utils.sendembed(bot, e, show_all=False)
         elif isinstance(error, discord.ApplicationCommandError):
-            e = discord.Embed(description=error, color=0xFF6969)
-            await utilz.sendembed(bot, e, show_all=False)
+            e = discord.Embed(description=f"`❌` {error}", color=0xFF6969)
+            await utils.sendembed(bot, e, show_all=False)
         raise error
 
     @commands.Cog.listener("on_command_error")
@@ -56,37 +50,59 @@ class Events(commands.Cog, name="Events"):
             raise error
         elif isinstance(error, commands.CommandError):
             e = discord.Embed(description=f"`❌` {error}", color=0xFF6969)
-            await utilz.sendembed(bot, e, delete=3)
+            await utils.sendembed(bot, e, delete=3)
         raise error
 
     @commands.Cog.listener("on_member_join")
     async def member_data(self, member):
-        with open('./data/afk.json', 'r') as f:
-            afk = json.load(f)
-        await other.OtherUtils.update_data(afk, member)
-        with open('./data/afk.json', 'w') as f:
-            json.dump(afk, f, indent=4, sort_keys=True)
+        afk = self.bot.afk
+        await users.UserUtils.update_data(afk, member)
+        configs.save(self.bot.afk_path, "w", afk)
 
     @commands.Cog.listener("on_guild_join")
     async def guild_add_data(self, guild):
-        with open('./data/prefixes.json', 'r') as f:
-            prefixes = json.load(f)
-        prefixes[str(guild.id)] = '$'
-        with open('./data/prefixes.json', 'w') as f:
-            json.dump(prefixes, f, indent=4)
+        prefixes = self.bot.guild_prefixes
+        prefixes[str(guild.id)] = '-'
+        configs.save(self.bot.guild_prefixes_path, "w", prefixes)
+
+        perms = self.bot.perms
+        perms[str(guild.id)] = {}
+        configs.save(self.bot.perms_path, "w", perms)
+
+        triggers = self.bot.triggers
+        triggers[str(guild.id)] = {}
+        triggers[str(guild.id)]["regex"] = {}
+        triggers[str(guild.id)]["regex"]["toggle"] = False
+        triggers[str(guild.id)]["regex"]["triggers"] = {}
+        triggers[str(guild.id)]["match"] = {}
+        triggers[str(guild.id)]["match"]["toggle"] = False
+        triggers[str(guild.id)]["match"]["triggers"] = {}
+        configs.save(self.bot.triggers_path, "w", triggers)
 
     @commands.Cog.listener("on_guild_remove")
     async def guild_remove_data(self, guild):
-        with open('./data/prefixes.json', 'r') as f:
-            prefixes = json.load(f)
+        prefixes = self.bot.guild_prefixes
         prefixes.pop(str(guild.id))
-        with open('./data/prefixes.json', 'w') as f:
-            json.dump(prefixes, f, indent=4)
+        configs.save(self.bot.guild_prefixes_path, "w", prefixes)
+
+        perms = self.bot.perms
+        perms.pop(str(guild.id))
+        configs.save(self.bot.perms_path, "w", perms)
+
+        triggers = self.bot.triggers
+        triggers.pop(str(guild.id))
+        configs.save(self.bot.triggers_path, "w", triggers)
+
+    @commands.Cog.listener("on_message")
+    async def memes_channel(self, message):
+        # delete messages in Northstar memes :dread:
+        if message.channel.id == 973438217196040242:
+            await message.delete(delay=random.randrange(100, 3600, 100))
 
     @commands.Cog.listener("on_message")
     async def afk_check(self, message):
         # check if user is afk or members in message
-        prefix = main.get_prefix(self, message)
+        prefix = main.get_prefix(self.bot, message)
         send = False
         afk_alert = discord.Embed(
             title=f"Members in your message are afk:")
@@ -94,19 +110,19 @@ class Events(commands.Cog, name="Events"):
             text=f"Toggle: {prefix}alerts\nDMs Toggle: {prefix}dmalerts")
         if message.author.bot:
             return
-        with open('./data/afk.json', 'r') as f:
-            afk = json.load(f)
+
         for member in message.mentions:
             if member.bot or member.id == message.author.id:
                 return
-            if afk[f'{member.id}']['AFK']:
+            if self.bot.afk[f'{member.id}']['AFK']:
                 send = True
 
                 # gets afk message
-                reason = afk[f'{member.id}']['reason']
+                reason = self.bot.afk[f'{member.id}']['reason']
 
                 # gets unix time
-                unix_time = int(time.time()) - int(afk[f'{member.id}']['time'])
+                unix_time = int(time.time()) - \
+                    int(self.bot.afk[f'{member.id}']['time'])
 
                 # user was afk for time.now() - time
                 afktime = humanize.naturaltime(
@@ -117,28 +133,27 @@ class Events(commands.Cog, name="Events"):
                     name=f"{member.display_name.replace('[AFK]', '')} - {afktime}", value=f"\"{reason}\"", inline=True)
 
                 # plus 1 time mentioned in afk.json
-                afk[f'{member.id}']['mentions'] = int(
-                    afk[f'{member.id}']['mentions']) + 1
+                self.bot.afk[f'{member.id}']['mentions'] = int(
+                    self.bot.afk[f'{member.id}']['mentions']) + 1
 
                 # save json
-                with open('./data/afk.json', 'w') as f:
-                    json.dump(afk, f, indent=4, sort_keys=True)
+                configs.save(self.bot.afk_path, 'w', self.bot.afk)
 
         if send:
-            await other.OtherUtils.sendafk(self, message, ["afk_alert", "afk_alert_dm"], afk_alert)
-        await other.OtherUtils.update_data(afk, message.author)
+            await users.UserUtils.sendafk(self, message, ["afk_alert", "afk_alert_dm"], afk_alert)
+        await users.UserUtils.update_data(self.bot.afk, message.author)
         # if message's author is afk continue
-        if list(message.content.split())[0] != f'{prefix}afk' and afk[f'{message.author.id}']['AFK']:
+        if list(message.content.split(" "))[0] != f'{prefix}afk' and self.bot.afk[f'{message.author.id}']['AFK']:
             # unix now - unix since afk
             timeafk = int(time.time()) - \
-                int(afk[f'{message.author.id}']['time'])
+                int(self.bot.afk[f'{message.author.id}']['time'])
 
             # make time readable for user
-            afktime = other.OtherUtils.period(datetime.timedelta(
+            afktime = users.UserUtils.period(datetime.timedelta(
                 seconds=round(timeafk)), "{d}d {h}h {m}m {s}s")
 
             # get mentions
-            mentionz = afk[f'{message.author.id}']['mentions']
+            mentionz = self.bot.afk[f'{message.author.id}']['mentions']
 
             # make embed
             welcome_back = discord.Embed(
@@ -150,12 +165,11 @@ class Events(commands.Cog, name="Events"):
                 text=f"Toggle: {prefix}wbalerts\nDMs Toggle: {prefix}wbdmalerts")
 
             # reset afk for user
-            afk[f'{message.author.id}']['AFK'] = False
-            afk[f'{message.author.id}']['reason'] = 'None'
-            afk[f'{message.author.id}']['time'] = '0'
-            afk[f'{message.author.id}']['mentions'] = 0
-            with open('./data/afk.json', 'w') as f:
-                json.dump(afk, f, indent=4, sort_keys=True)
+            self.bot.afk[f'{message.author.id}']['AFK'] = False
+            self.bot.afk[f'{message.author.id}']['reason'] = 'None'
+            self.bot.afk[f'{message.author.id}']['time'] = '0'
+            self.bot.afk[f'{message.author.id}']['mentions'] = 0
+            configs.save(self.bot.afk_path, 'w', self.bot.afk)
 
             # try to reset nickname
             try:
@@ -165,24 +179,30 @@ class Events(commands.Cog, name="Events"):
                 print(
                     f'I wasnt able to edit [{message.author} / {message.author.id}].')
 
-            await other.OtherUtils.sendafk(self, message, ["wb_alert", "wb_alert_dm"], welcome_back)
-        with open('./data/afk.json', 'w') as f:
-            json.dump(afk, f, indent=4, sort_keys=True)
+            await users.UserUtils.sendafk(self, message, ["wb_alert", "wb_alert_dm"], welcome_back)
+        configs.save(self.bot.afk_path, 'w', self.bot.afk)
 
     @commands.Cog.listener("on_message")
     async def help_check(self, message):
         # check if user's message is only bot ping and reply with help, if not process commands
         if message.author.bot == False and self.bot.user.mentioned_in(message) and len(message.content) == len(self.bot.user.mention):
-            await message.reply(embed=discord.Embed(description=f'My prefix is `{main.get_prefix(self, message)}`, you can also use slash commands\nFor more info use the {main.get_prefix(self, message)}help command!'), delete_after=20, mention_author=False)
+            await message.reply(embed=discord.Embed(description=f'My prefix is `{self.bot.guild_prefixes[str(message.guild.id)]}` or {self.bot.user.mention}, you can also use slash commands\nFor more info use the /help command!'), delete_after=20, mention_author=False)
         else:
             await self.bot.process_commands(message)
 
     @commands.Cog.listener("on_message")
+    async def word_triggers(self, message):
+        if message.author.bot or isinstance(message.channel, discord.DMChannel):
+            return
+        await self.trigger(message, "match")
+        await self.trigger(message, "regex")
+        await self.replies(message)
+
     async def replies(self, message):
         global counter
         # automatic responses
         if "eulog" in message.content.lower():
-            await message.add_reaction(self.eulogy_emoji)
+            await message.add_reaction(self.bot.eulogy_emoji)
             counter += 1
         if "cleansing pool" in message.content.lower():
             await message.reply("Watch your language.")
@@ -190,55 +210,44 @@ class Events(commands.Cog, name="Events"):
         if not message.author.bot:
             # "ask newt" functionality
             if self.bot.user.mentioned_in(message) and len(message.content) >= len(self.bot.user.mention):
-                await message.reply(random.choice(self.convo_replies))
-            # survivor responses
-            annoying = {"mando": "Ew.",
-                        "huntress": "Her burden :flushed:",
-                        "bandit": "Bandit makes some mighty fine beans.",
-                        "engi": "<:BUNGUS:977317901277212722> :heart: ***B U N G U S*** :heart: <:BUNGUS:977317901277212722>",
-                        "arti": "Ion surge OP, pls nerf",
-                        "merc": "I simply never get hit as i utilise my I-frames perfectly leaving no opening to be hit by enemy attacks, and if you dislike merc you simply have a massive skill issue and wasted your time trying to get bitches instead of studying the blade",
-                        "rex": "Knock knock \n who's there? \n A concussion from a high amplitude sonic boom ⨉ What's pink and green and is about to show you the definition of pain?",
-                        "loader": "Beat mitrhix into a bloody pulp any% speedrun",
-                        "acrid": ":heart Acid doggy :heart:",
-                        "cap": "Thermonuclear warfare :thumbsup:",
-                        "gunner": "Epic railer MLG quickscope compilation #69",
-                        "fiend viend": "I LOVE LEAN",
-                        "goat ⨉ hoof ⨉ drink": """Go, go, go, go, go, go, go\n\nGotta go fast, gotta go fast\nGotta go faster, faster, faster, faster, faster!\n\nMovin' at speed of sound (make tracks!)\nQuickest hedgehog around\nGot ourselves a situation\nStuck in a new location\nWithout any explanation\nNo time for relaxation!\n\nDon't, don't, don't, don't, don't blink, don't think\nJust go, go, go, go, g-g-g-g-go, go!\n\nN-n-n-n-n-n-na\nN-n-n-n-n-n-na\n\nSonic, he's on the run!\nSonic, he's number one!\nSonic, he's comin' next!\nSo watch out... For Sonic X!\n\nGotta go fast (Sonic!)\nGotta go fast (Sonic!)\nGotta go faster, faster, faster, faster, faster!\nGo, go, go, go, go, go, go\nSonic X!\n\nGotta go fast, gotta go fast\nGotta go faster, faster, faster, faster, faster!\nSonic X!""",
-                        "mul-t ⨉ mult ⨉ multi": "Haha MUL-T double nail go ***brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr***",
-                        "nya ⨉ meow": "Wowwwww, you meow like a cat! That means you are one, right? Shut the fuck up. If you want to be put on a leash and treated like a domestic animal, that's called a fetish, not “quirky” or “cute.” What part of you seriously thinks that any portion of acting like a feline establishes a reputation of appreciation? Is it your lack of any defining aspect of personality that urges you to resort to shitty representations of cats to create an illusion of meaning in your worthless life? Wearing “cat ears” in the shape of headbands further notes the complete absence of human attribution to your false sense of personality, such as intelligence or charisma in any form or shape. Where do you think this mindset's going to lead you? Do you think you're funny, random, quirky even? What makes you think that acting like a fucking cat will make a goddamn hyena laugh? I, personally, feel highly sympathetic towards you as your only escape from the worthless thing you call your existence is to pretend to be an animal. But it's not a worthy choice to assert this horrifying fact as a dominant trait, mainly because personality traits require an initial personality to lay their foundation on. You're not worthy of anybody's time, so go fuck off, \"cat-girl.\""
-                        }
-
-            for trigger, reply in annoying.items():
-                multi_trigger = list(trigger.split(' ⨉ '))
-                for triggers in multi_trigger:
-                    if triggers in message.content.lower():
-                        reply = random.choice(list(reply.split(' ⨉ ')))
-                        await message.reply(reply)
+                await message.reply(random.choice(self.bot.convo_replies))
 
         # handle lunar coins dropping
         if random.randint(1, 20) <= 3 and message.author.bot == False:
-            await message.add_reaction(self.lunar_coin_emoji)
+            await message.add_reaction(self.bot.lunar_coin_emoji)
 
             try:
-
-                self.save[message.author.id]["coins"] += 1
+                self.bot.save[message.author.id]["coins"] += 1
             except:
-                self.save[message.author.id] = {
+                self.bot.save[message.author.id] = {
                     "coins": 1,
                     "eulogies": 0
                 }
+
+    async def trigger(self, message, type: str):
+        if type == "regex":
+            msg = message.content.lower()
+        else:
+            msg = message.content.split(" ")
+        if self.bot.triggers[str(message.guild.id)][type]["toggle"]:
+            for trigger, reply in self.bot.triggers[str(message.guild.id)][type]["triggers"].items():
+                multi_trigger = list(trigger.split('|'))
+                for triggers in multi_trigger:
+                    if triggers in msg:
+                        reply = random.choice(list(reply.split('|')))
+                        await message.reply(reply)
+                        break
 
     @tasks.loop(minutes=1)
     async def autosave(self):
         print("Saving to disk...", end=' ')
 
         with open("./data/save.json", mode="w", encoding="utf-8") as savefile:
-            savefile.write(json.dumps(self.save, sort_keys=True, indent=4))
+            savefile.write(json.dumps(self.bot.save, sort_keys=True, indent=4))
             savefile.flush()
 
         with open("./data/hugs.txt", mode="w", encoding="utf-8") as file:
-            file.write(str(self.hugs))
+            file.write(str(self.bot.hugs))
             file.flush()
 
         print("Done!")
